bb5bba4c2b07f35fae0aee4edee2f8a1ee08738c
MitchellWestcott <westcott.mitchell@gmail.com>
2020-12-04 18:24:48 -0500

    Fixed a typo

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-
 import GlobalStyles from './GlobalStyles';
+import styled from 'styled-components'
+
+import data from "../data";
+import Typeahead from "./Typeahead.js"
 
 const App = (props) => {
   return (
     <>
       <GlobalStyles />
       {/* TODO */}
+      <Wrapper>
+        <Typeahead 
+        suggestions={data.books}
+        categories={data.categories}
+        handleSelect={(suggestion) => {
+          window.alert(suggestion)
+        }}
+      />
+      </Wrapper>
     </>
   );
 };
 
 export default App;
+
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
